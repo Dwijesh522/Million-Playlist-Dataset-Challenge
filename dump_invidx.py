68b586294d7070ce68aa3a1a53b95ba1b2cecc93
dwijesh522 <iddwijesh@gmail.com>
2020-12-19 12:47:09 +0530

    dumps invidx to disk

--- dump_invidx.py
+++ dump_invidx.py
@@ -3,13 +3,17 @@
     expected to have mpd.slice.*.json files. The program
     generates on-disk inverted index of the form:
         { track: {playlist : freq} }
-        { pid: last_modified}
+        { numpy.ndarray(last_modified)}
+        { numpy.ndarray(playlist length)}
 """
 
 import json
 import sys
 from os import listdir
 from os.path import isfile, join
+import numpy as np
+
+TOTAL_DOCS = 1000000
 
 def error(l):
     if (len(l) != 2):
@@ -21,10 +25,16 @@ def dumpToJson(dt, filename):
     with open(filename, 'w') as f_obj:
         json.dump(dt, f_obj)
 
+def dumpToNpy(dt, filename):
+    """ dump dictionary dt to npy file"""
+    np.save(filename, dt)
+
 def dump_invidx(slices_path):
     slices = [join(slices_path, f) for f in listdir(slices_path) if isfile(join(slices_path, f))]
     invidx = {}
-    modifications = {}
+    modifications = np.empty(TOTAL_DOCS, dtype = int)
+    tracks_per_pid = np.empty(TOTAL_DOCS, dtype=int)
+
     for f in slices:
         print(f)
         with open(f, 'r') as file_obj:
@@ -37,6 +47,7 @@ def dump_invidx(slices_path):
                 pid = playlist['pid']
                 modified_at = playlist['modified_at']
                 modifications[pid] = modified_at
+                tracks_per_pid[pid] = playlist['num_tracks']
                 tracks = playlist['tracks'] # list of dicts
                 for track in tracks:
                     track_uri = track['track_uri']
@@ -49,7 +60,8 @@ def dump_invidx(slices_path):
                         invidx[track_uri] = {pid: 1}
     # writting to file
     dumpToJson(invidx, "invidx.json")
-    dumpToJson(modifications, "modifications.json")
+    dumpToNpy(modifications, "modifications.npy")
+    dumpToNpy(tracks_per_pid, "tracks_per_pid.npy")
 
 if __name__ == "__main__":
     error(sys.argv)
